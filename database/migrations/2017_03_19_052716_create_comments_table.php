33f8226f4528bdbdf903c14086fcadeebb196f30
Zain ul Hassan <zainulhassan4@gmail.com>
2017-03-19 18:43:04 +1000

    Post and Comment: migration

---
+++ database/migrations/2017_03_19_052716_create_comments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('post_id');
+            $table->integer('user_id');
+            $table->string('comment', 500);
+            $table->boolean('active')->default(1);
+            $table->timestampsTz();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
