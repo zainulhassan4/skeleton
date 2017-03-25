b7e8e05d47e732c288ceb9a946203f47e34d6d3b
Zain ul Hassan <zainulhassan4@gmail.com>
2017-03-25 21:12:21 +1000

    Comment: save function to add author

--- app/Comment.php
+++ app/Comment.php
@@ -3,10 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 
 class Comment extends Model
-{
+{    
+    use Translatable;
+
+    protected $translatable = ['admin_remark', 'body'];
+    
     /**
      * Always eager load the author with the comment
      * @var array
@@ -19,5 +24,31 @@ class Comment extends Model
     public function user()
     {
         return $this->belongsTo('App\User');
+    }
+
+    /**
+     * 
+     * @param array $options
+     */
+    public function save(array $options = [])
+    {
+        // If no author has been assigned, assign the current user's id as the author of the post
+        if (!$this->user_id && Auth::user()) {
+            $this->user_id = Auth::user()->id;
+        }
+
+        parent::save();
+    }
+
+    /**
+     * Scope a query to only include active comments.
+     *
+     * @param  $query  \Illuminate\Database\Eloquent\Builder
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeActive($query)
+    {
+        return $query->where('active', true);
     }
 }
